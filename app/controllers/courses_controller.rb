99f70384113ee4587c35d9e30a319c15ea1b8a2c
Nguyen Van Duc <duc.nv226@gmail.com>
2015-01-28 09:50:36 +0700

    add/remove course without login user
    edit course
    
    repair commented

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,50 @@
+class CoursesController < ApplicationController
+  def new
+  	@course = Course.new
+  end
+
+  def show
+  	@course = Course.find(params[:id])
+  end
+
+  def create
+  	@course = Course.new(course_params)
+  	if @course.save
+  		flash[:success] = 'Added new course.'
+  		redirect_to @course
+  		return
+  	else
+  		render 'new'
+  	end
+  end
+
+  def edit
+  	@course = Course.find(params[:id])
+  end
+
+  def update
+  	@course = Course.find(params[:id])
+  	if @course.update_attributes(course_params)
+  			flash[:success] = 'Updated course.'
+  			redirect_to @course
+  	else
+  		render 'edit'
+  	end
+
+  end
+
+  def index
+  	@courses = Course.paginate(page: params[:page])
+  end
+
+  def destroy
+  	Course.find(params[:id]).destroy
+  	flash[:success] = 'Deleted course.'
+  	redirect_to courses_url
+  end
+
+private 
+  	def course_params
+  		params.require(:course).permit(:name)
+  	end
+end
